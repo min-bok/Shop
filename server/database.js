af71358610f0fab91b2ada1a5f233f04c4acb92a
John Doe <leehy0782@naver.com>
2022-12-28 00:48:40 +0900

    Feat: 데이터베이스 연결 수정
    로컬 DB에서 Heroku에 배포된 DB로 연결된 DB를 수정

--- server/database.js
+++ server/database.js
@@ -1,9 +1,5 @@
-import fs from "fs";
 import mysql from "mysql2/promise";
 import option from "./option.js";
-
-const data = fs.readFileSync("./database.json");
-const conf = JSON.parse(data);
 
 const pool = mysql.createPool(option);
 
