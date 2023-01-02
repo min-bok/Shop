780f628933f89ff6acf214682ed7e1cb3c2fb836
John Doe <leehy0782@naver.com>
2023-01-03 00:17:30 +0900

    re-deploy

--- server/option.js
+++ server/option.js
@@ -1,25 +1,25 @@
 import dotenv from "dotenv";
 dotenv.config();
-import fs from "fs";
+// import fs from "fs";
 // dev
-const data = fs.readFileSync("./database.json");
-const conf = JSON.parse(data);
+// const data = fs.readFileSync("./database.json");
+// const conf = JSON.parse(data);
 
-const option = {
-  host: conf.host,
-  user: conf.user,
-  password: conf.password,
-  port: conf.port,
-  database: conf.database,
-};
+// const option = {
+//   host: conf.host,
+//   user: conf.user,
+//   password: conf.password,
+//   port: conf.port,
+//   database: conf.database,
+// };
 
 // prod
-// const option = {
-//   host: process.env.host,
-//   user: process.env.user,
-//   password: process.env.password,
-//   port: process.env.port,
-//   database: process.env.database,
-// };
+const option = {
+  host: process.env.host,
+  user: process.env.user,
+  password: process.env.password,
+  port: process.env.port,
+  database: process.env.database,
+};
 
 export default option;
