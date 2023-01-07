a729ab36edb8f40bf7361129586e4a7a9b8d499d
John Doe <leehy0782@naver.com>
2023-01-07 18:36:36 +0900

    deploy

--- client/src/components/AdSlider.js
+++ client/src/components/AdSlider.js
@@ -3,6 +3,8 @@ import Slider from "react-slick";
 import "../style/slick.css";
 import "../style/slick-theme.css";
 
+import img from "../assets/adex.png";
+
 export default class SimpleSlider extends Component {
   render() {
     const settings = {
@@ -12,26 +14,15 @@ export default class SimpleSlider extends Component {
       slidesToShow: 1,
       slidesToScroll: 1,
     };
+
     return (
       <div>
         <Slider {...settings}>
           <div>
-            <h3>1</h3>
+            <img src={img} alt="" />
           </div>
           <div>
             <h3>2</h3>
-          </div>
-          <div>
-            <h3>3</h3>
-          </div>
-          <div>
-            <h3>4</h3>
-          </div>
-          <div>
-            <h3>5</h3>
-          </div>
-          <div>
-            <h3>6</h3>
           </div>
         </Slider>
       </div>
